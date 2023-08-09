58c1fc4737de52cf3d21dbe56bdc1a31e69096d3
Luiz Henrique <lh.m21112000@gmail.com>
2023-08-09 11:02:42 -0300

    atualizacao

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import * as ReactBootstrap from "react-bootstrap";
 import "../styles/App.css";
 
+import logo from "../img/logo_struct.png";
+
 function Home() {
   return (
     <>
@@ -12,7 +14,7 @@ function Home() {
             <a href="/">
               <ReactBootstrap.Image
                 className="logo"
-                src='#'
+                src={logo}
                 alt="Logo"
                 title="Logo"
               />
@@ -40,6 +42,21 @@ function Home() {
           </ReactBootstrap.Container>
         </ReactBootstrap.Navbar>
       </header>
+      <section>
+        <div className="fundoMainFrame">
+          <div className="mainFrame">
+            <ReactBootstrap.Container>
+              <div className="textos">
+                <h4>Texto pequeno de efeito!</h4>
+                <h1>TEXTO GRANDE CHAMATIVO </h1>
+                <h1>Slogam</h1>
+                <ReactBootstrap.Button variant="secondary">Veja meus projetos</ReactBootstrap.Button>
+              </div>
+              
+            </ReactBootstrap.Container>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
